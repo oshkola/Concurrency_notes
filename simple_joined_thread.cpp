47daa6962236f0e5da1ff8f9ab47457965c1ee30
Oleg Shkola <olsh91@gmail.com>
2020-02-29 15:55:55 +0200

    Create simple_joined_thread.cpp
    Simple thread with join()

---
+++ simple_joined_thread.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unistd.h>
+#include <thread>
+
+void doWork()
+{
+	std::cout << "******DOWORK STARTED****** \n";
+
+	for (int i = 1; i <= 20; i++)
+	{
+		std::cout << "doWork(): \t" << i << "\n";
+		usleep(1000000);
+	}
+
+	std::cout << "******DOWORK() FINISHED****** \n";
+}
+
+int main()
+{
+	std::thread th1(doWork);
+	th1.detach();
+
+	for (int i = 1; i <= 20; i++)
+	{
+		std::cout << "main(): \t" << i << "\n";
+		usleep(500000);
+	}
+
+}
