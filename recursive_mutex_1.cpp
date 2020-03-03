66cbbb2eb57b6d1f730aebde37967e179b0b772d
Oleg Shkola <olsh91@gmail.com>
2020-03-03 16:09:55 +0200

    Create recursive_mutex_1.cpp

---
+++ recursive_mutex_1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void decreaseValue(int a)
+{
+    
+std::cout << a << " ";
+
+std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+
+if (a <= 1)
+{
+	std::cout << "\n";
+	return;
+}
+--a;
+
+decreaseValue(a);
+}
+
+int main()
+{
+	std::thread th1(decreaseValue, 10);
+
+	std::thread th2(decreaseValue, 10);
+
+	th1.join();
+	th2.join();
+}
