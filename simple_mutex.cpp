56f7a0faedadd273badba5e92c33a5a4c89ff55a
Oleg Shkola <olsh91@gmail.com>
2020-03-03 11:48:05 +0200

    Create simple_mutex.cpp

---
+++ simple_mutex.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+std::mutex mtx;
+
+void print(char smb)
+{
+	mtx.lock();
+
+	for(int i = 0; i < 5; i++)
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			std::cout << smb;
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	std::cout << "\n";
+	}
+	mtx.unlock();
+}
+
+int main()
+{
+	std::thread th1(print, '*');
+    std::thread th2(print, '#');
+
+	th1.join();
+	th2.join();
+}
