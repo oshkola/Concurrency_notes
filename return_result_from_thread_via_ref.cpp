cc1926cbf36143aac938c9bbd6e6e3125cd01ee9
Oleg Shkola <olsh91@gmail.com>
2020-03-04 13:41:06 +0200

    Update return_result_from_thread_via_ref.cpp

--- return_result_from_thread_via_ref.cpp
+++ return_result_from_thread_via_ref.cpp
@@ -4,7 +4,7 @@
 
 void changeInput(int &a)
 {
-	std::cout << "******CHANGEINPUT STARTED****** \n";
+	std::cout << "******CHANGEINPUT STARTED******" << std::endl;
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
@@ -12,18 +12,18 @@ void changeInput(int &a)
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-	std::cout << "******DOWORK() FINISHED****** \n";
+	std::cout << "******CHANGEINPUT FINISHED******" << std::endl;
 }
 
 int main()
 {
 	int a = 7;
 
-	std::cout << "1 main(): a = " << a << "\n";
+	std::cout << "1 main(): a = " << a << std::endl;
 
 	std::thread th1(changeInput, std::ref(a));
 
 	th1.join();
 
-	std::cout << "2 main(): a = " << a << "\n";
+	std::cout << "2 main(): a = " << a << std::endl;
 }
