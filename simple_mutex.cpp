8e9a0797a5f2cbaeedffa56ad04bd60759b31c74
Oleg Shkola <olsh91@gmail.com>
2020-03-04 13:43:19 +0200

    Update simple_mutex.cpp

--- simple_mutex.cpp
+++ simple_mutex.cpp
@@ -16,14 +16,14 @@ void print(char smb)
 			std::cout << smb;
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
-	std::cout << "\n";
+	std::cout << std::endl;
 	}
 	mtx.unlock();
 }
 
 int main()
 {
-	std::thread th1(print, '*');
+std::thread th1(print, '*');
     std::thread th2(print, '#');
 
 	th1.join();
